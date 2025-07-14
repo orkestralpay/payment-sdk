d0c8cf6f817c96ee9ec76606b67f93bf92d52352
Oberdan Soldi <oberdan_soldi@outlook.com.br>
2025-07-14 11:48:26 -0300

    refactor<ORK-1264>: update ESLint and TypeScript configurations, add vite-plugin-dts, and remove unused components
    - Enhanced ESLint configuration by refining rules and adjusting file ignores.
    - Updated TypeScript configuration to include new files and JSX support.
    - Integrated vite-plugin-dts for improved type declaration generation.
    - Removed unused App and main components to clean up the project structure.
    - Adjusted package.json to reflect changes in project structure and dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -7,101 +7,111 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import perfectionist from "eslint-plugin-perfectionist";
 
 export default tseslint.config(
-  { ignores: ["dist", "jest.config.ts", "vite.config.ts"] },
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      prettier: prettier,
-      "react-hooks": reactHooks,
-      perfectionist: perfectionist,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "prettier/prettier": "error",
-      "react-hooks/exhaustive-deps": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/interface-name-prefix": "off",
-      "@typescript-eslint/no-unsafe-function-type": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "perfectionist/sort-maps": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-enums": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-exports": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-objects": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "perfectionist/sort-jsx-props": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-interfaces": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-union-types": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-object-types": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-named-imports": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-named-exports": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-array-includes": [
-        "error",
-        { order: "asc", type: "line-length" },
-      ],
-      "perfectionist/sort-imports": [
-        "error",
-        {
-          order: "asc",
-          type: "line-length",
-          internalPattern: ["^~/"],
-          newlinesBetween: "always",
-          groups: [
-            "type",
-            ["react", "builtin", "external"],
-            ["internal", "parent", "sibling", "index"],
-          ],
-          customGroups: {
-            type: {
-              react: ["^react$", "^react-."],
-            },
-            value: {
-              lodash: "lodash",
-              react: ["^react$", "^react-."],
-            },
-          },
-        },
-      ],
-    },
-  }
+	{ ignores: ["dist", "jest.config.ts"] },
+	{
+		files: ["vite.config.ts", "jest.config.ts"],
+		languageOptions: {
+			globals: globals.node
+		},
+		rules: {
+			"perfectionist/sort-imports": "off",
+			"perfectionist/sort-objects": "off"
+		}
+	},
+	{
+		files: ["**/*.{ts,tsx}"],
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			prettier: prettier,
+			"react-hooks": reactHooks,
+			perfectionist: perfectionist,
+			"react-refresh": reactRefresh,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"prettier/prettier": "error",
+			"react-hooks/exhaustive-deps": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-empty-object-type": "off",
+			"@typescript-eslint/interface-name-prefix": "off",
+			"@typescript-eslint/no-unsafe-function-type": "off",
+			"@typescript-eslint/explicit-function-return-type": "off",
+			"@typescript-eslint/explicit-module-boundary-types": "off",
+			"perfectionist/sort-maps": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-enums": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-exports": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-objects": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"react-refresh/only-export-components": [
+				"warn",
+				{ allowConstantExport: true },
+			],
+			"perfectionist/sort-jsx-props": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-interfaces": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-union-types": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-object-types": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-named-imports": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-named-exports": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-array-includes": [
+				"error",
+				{ order: "asc", type: "line-length" },
+			],
+			"perfectionist/sort-imports": [
+				"error",
+				{
+					order: "asc",
+					type: "line-length",
+					internalPattern: ["^~/"],
+					newlinesBetween: "always",
+					groups: [
+						"type",
+						["react", "builtin", "external"],
+						["internal", "parent", "sibling", "index"],
+					],
+					customGroups: {
+						type: {
+							react: ["^react$", "^react-."],
+						},
+						value: {
+							lodash: "lodash",
+							react: ["^react$", "^react-."],
+						},
+					},
+				},
+			],
+		},
+	}
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,40 @@
 import path from 'path';
+import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react-swc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
 	resolve: {
 		alias: {
-			'~': path.resolve(__dirname, './src')
+			'~': path.resolve(__dirname, 'src')
+		}
+	},
+	plugins: [
+		react(),
+		dts({
+			insertTypesEntry: true,
+			tsconfigPath: './tsconfig.app.json'
+		})
+	],
+	build: {
+		lib: {
+			name: 'orkestralpay-react',
+			fileName: 'orkestralpay-react',
+			entry: path.resolve(__dirname, 'src/index.ts')
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM'
+				}
+			}
 		}
 	}
 });
